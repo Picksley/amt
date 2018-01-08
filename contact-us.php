ea85ab1dbfd58e781afefb76f063525d005eeaca
Picksley <david@picksley.in>
2018-01-08 19:58:31 +0000

    8pm upload
    Added some content to the contact page, fixed a couple bugs

--- contact-us.php
+++ contact-us.php
@@ -24,22 +24,21 @@
 				<textarea type="text" name="message" rows="10" placeholder="Tell us a bit more about your enquiry and we will answer when we reply..." ></textarea>
 				<span><input type="checkbox" class="ct-checkbox" checked></input><strong> Marketing Communications Agreement</strong> <br/>
 				<p class="checkbox-terms">By entering your details above you are consenting to receive emails and SMS containing offers and informational updates from AMT Contract Hire and Leasing. To opt-out from any marketing communications, please tick this box. Your details will not be shared with any third-parties. For more information please read our <a href="privacy-policy.php" target="_blank">Privacy Policy</a> for further details.</p></span>
-
 				<button type="submit" class="btn btn-ct btn-default pull-right">SUBMIT ENQUIRY <img src="images/hmright_arrow.png"></img></button>
 			</form>
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-6 ct-more-info">
 			<h2> Want to talk? </h2>
 
-			<h3> Our details </h3>
+			<h4> Our details </h4>
 
-			<span class="contact-icon"><img src="images/icons/phone.svg"/> <strong> 0113 350 5979 </strong> </span> <br/>
+			<p><span class="contact-icon"><img src="images/icons/phone.svg"/> <strong> 0113 350 5979 </strong> </span></p>
 
-			<span class="contact-icon"><img src="images/icons/email.svg"/> <strong> Leasing sales enquiries: </strong> <a href="mailto:newenq@amtleasing.co.uk"> newenq@amtleasing.co.uk </a> </span><br/>
-			<span class="contact-icon"><img class="no-visibility" src="images/icons/email.svg"/> <strong> Administration team: </strong> <a href="mailto:admin@amtleasing.co.uk"> admin@amtleasing.co.uk </a> </span><br/>
-			<span class="contact-icon"><img class="no-visibility" src="images/icons/email.svg"/> <strong> Accounts: </strong> <a href="mailto:accounts@amtleasing.co.uk"> accounts@amtleasing.co.uk </a> </span>	<br/>
-			<span class="contact-icon"><img class="" src="images/icons/email.svg"/> <strong> Mon-Fri </strong> 9am - 5.30pm</span><br />
-			<span class="contact-icon"><img class="no-visibility" src="images/icons/email.svg"/> <strong> Sat-Sun </strong> Closed</span>
+			<p><span class="contact-icon"><img src="images/icons/email.svg"/> <strong> Leasing sales enquiries: </strong> <a href="mailto:newenq@amtleasing.co.uk"> newenq@amtleasing.co.uk </a> </span></p>
+			<p><span class="contact-icon"><img class="no-visibility" src="images/icons/email.svg"/> <strong> Administration team: </strong> <a href="mailto:admin@amtleasing.co.uk"> admin@amtleasing.co.uk </a> </span></p>
+			<p><span class="contact-icon"><img class="no-visibility" src="images/icons/email.svg"/> <strong> Accounts: </strong> <a href="mailto:accounts@amtleasing.co.uk"> accounts@amtleasing.co.uk </a> </span></p>
+			<p><span class="contact-icon"><img class="" src="images/icons/email.svg"/> <strong> Mon-Fri </strong> 9am - 5.30pm</span></p>
+			<p><span class="contact-icon"><img class="no-visibility" src="images/icons/email.svg"/> <strong> Sat-Sun </strong> Closed</span></p>
 		</div>
 	</div>
 </section>
