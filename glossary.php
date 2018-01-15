5f0e6dfeadebed0200785409504770e2dd430f50
Picksley <david@picksley.in>
2018-01-15 17:01:16 +0000

    Regular Update
    Regular Update

---
+++ glossary.php
@@ -0,0 +1,85 @@
+<?php include('header.php'); ?>
+<div class="container">
+  <div class="col-md-12">
+    <div class="page-title-dark">
+      <h1> Glossary </h1>
+      <p> The leasing industry is awash with jargon and acronyms! We want to be as transparent as possible so we've put together a handy glossary to help you out. </p>
+    </div>
+  </div>
+</div>
+<section class="glossary_content">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12 text-center glossary_subtitle"><h3>Select a term to be taken to the description </h3></div>
+
+      <div class="glossary_nav">
+        <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4">
+          <h4> A - C </h4>
+
+          <a href="#lipsum1"> Lorem Ipsum </a>
+
+        </div>
+
+        <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4">
+          <h4> D - K </h4>
+
+          <a href="#lipsum2"> Lorem Ipsum </a>
+
+        </div>
+        <div class="col-md-4 col-xs-4 col-lg-4 col-sm-4">
+          <h4> L - Z </h4>
+
+          <a href="#lipsum3"> Lorem Ipsum </a>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</section>
+<section class="glossary_terms">
+<div class="container">
+
+  <div class="glossary_items">
+
+  <div class="glossary_item" id="lipsum1">
+    <h3> Lorem Ipsum </h3>
+    <p> As part of your annual leasing contract you will agree to stick to an annual mileage limit. This is because the mileage you do is closely linked to the depreciation of the vehicle, and the monthly fee you pay is for the depreciation of the vehicle, and the monthly fee you pay is for the depreciation of the vehicle over time. The annual mileage is normally anything from 5,000 to 50,000 miles per year. </p>
+  </div>
+
+  <div class="glossary_item" id="lipsum2">
+    <h3> Lorem Ipsum </h3>
+    <p> As part of your annual leasing contract you will agree to stick to an annual mileage limit. This is because the mileage you do is closely linked to the depreciation of the vehicle, and the monthly fee you pay is for the depreciation of the vehicle, and the monthly fee you pay is for the depreciation of the vehicle over time. The annual mileage is normally anything from 5,000 to 50,000 miles per year. </p>
+  </div>
+
+  <div class="glossary_item" id="lipsum3">
+    <h3> Lorem Ipsum </h3>
+    <p> As part of your annual leasing contract you will agree to stick to an annual mileage limit. This is because the mileage you do is closely linked to the depreciation of the vehicle, and the monthly fee you pay is for the depreciation of the vehicle, and the monthly fee you pay is for the depreciation of the vehicle over time. The annual mileage is normally anything from 5,000 to 50,000 miles per year. </p>
+  </div>
+
+
+</div>
+
+</div>
+</section>
+
+
+<section class="customers-col">
+<div class="container">
+<div class="col-md-12">
+<div class="title2">
+<h1> Your Recently Viewed Vehicles </h1>
+</div>
+</div>
+</div>
+
+<?php include('recently_viewed_vehicles.php'); ?>
+</section>
+
+
+<?php include('newsletter.php'); ?>
+
+
+
+<?php include('footer.php'); ?>
